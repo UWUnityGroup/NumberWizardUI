63810aca714ee74bcb9a53b66585ddd827a698fb
Daeyoun Kim <Daeyoun Kim>
2016-03-31 13:27:49 -0500

    Initial commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string level)
+    {
+        Debug.Log(level);
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+    public Text guessText, guessLeftText;
+    public int maxGuessesAllowed = 10;
+
+    int max, min, guess;
+	
+	// Use this for initialization
+	void Start () {	
+		StartGame ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+
+        GuessNumber();
+	}
+
+    public void GuessHigher()
+    {
+        min = guess;
+        GuessNumber();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        GuessNumber();
+    }
+
+    public void GuessCorrect()
+    {
+        SceneManager.LoadScene("Lose");
+    }
+
+    void GuessNumber () {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+
+        maxGuessesAllowed--;
+        guessLeftText.text = maxGuessesAllowed.ToString();
+
+        if (maxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
